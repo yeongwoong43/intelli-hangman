531482567caa3707fdeff2004a27e6c155c889c2
FoxTrott <yeongwoong43@gmail.com>
2023-03-31 23:44:45 +0900

    feat: Finish to implement master.py
    일단 model과 STATE를 주고 받는 형식의 master.py 구현은 끝남.
    이제 뼈대가 완성되었으니 디버깅만 마무리되면 모델 구현으로 넘어가면 될 듯

--- pycode/master.py
+++ pycode/master.py
@@ -2,15 +2,6 @@ import random
 import numpy as np
 import pandas as pd
 
-# 클래스로 엮는 중 (미완성)
-
-class ModelDesignError(Exception):
-    def __init__(self, msg):
-        self.msg = "ModelDesignError: Why try same letter in game again?"
-    
-    def __str__(self):
-        return self.msg
-
 class Master:
 
     def __init__(self, wordList=None, MAX_LEN=64):
@@ -22,14 +13,13 @@ class Master:
         else: self.wordList=wordList
 
         self.MAX_LEN = MAX_LEN
-        self.STATE = []
-        self.time = 0
+        self.time = -1
 
     def word_to_df(self, word, test=False): # 단어를 state꼴로 바꾸는 method
         n = len(word)
 
         df = pd.DataFrame(np.zeros((self.MAX_LEN, 27)))
-        df.index = ['hash']+[chr(i) for i in range(97, 123)]
+        df.columns = ['hash']+[chr(i) for i in range(97, 123)]
         df['hash'] = [1 for i in range(n)] + [0 for i in range(64-n)]
         if not test:
             for i in range(n):
@@ -44,39 +34,49 @@ class Master:
 
 
     def stepNext(self, ans): # 답안하고 이전 state를 받아서 다음 state 리턴
-        if self.time == 0:
-            self.goal = [self.selectWord()]
-            self.goal.append(self.word_to_df(self.goal))
-            self.STATE.append(self.word_to_df(self.goal, test=True))
+        if self.time == -1: raise Exception("Why don't you call the method initGame() first?")
         
+        elif (self.STATE[self.time] == self.goal[1]).all().all():
+            return (-1, self.time) # 다 풀면 원래의 time란에 -1 대입하도록 했음
+
         else:
             if sum(self.STATE[self.time][ans]!=0) == 0: # 시도한 적 없는 글자라면
                 new_state = self.STATE[self.time].copy()
                 if ans in self.goal[0]:
                     new_state[ans] = self.goal[1][ans]
+                    # 공사중
+                    # def markClue(row):
+                    #     if row[ans] == 1:
+                    #         for char in [chr(i) for i in range(ord('a'), ord('z')+1)].remove(ans):
+                    #             row[char] = -1
+                    #     return row
+                    new_state = new_state.apply(markClue)
+                        
+                
+                else:
+                    # 공사중
+                    # def markClue(row):
+                    #     if row['hash'] == 1:
+                    #         row[ans] = -1
+                    #     return row
+                    new_state = new_state.apply(markClue)
+                
+                self.STATE.append(new_state)
+                self.time += 1
                     
             
             else: # 이미 한 번 했던 질문을 반복할 경우
-                raise ModelDesignError
+                raise Exception("ModelDesignError: Why try same letter again?")
 
 
-        return self.STATE[self.time]
+        return (self.time, self.STATE[self.time])
 
     
 
-    def playGame(self):
+    def initGame(self):
 
         # 답 생성
-        self.goal = self.selectWord()
-        self.STATE.append(self.word_to_df(self.goal))
-
-
-        # while loop
-        while True:
-
-            # state 전송
-            
-
-            # model에서 ans 수신
-
-            # 새 state 계산
+        goal = self.selectWord()
+        self.goal = [goal, self.word_to_df(goal)]
+        self.STATE = [self.word_to_df(goal, test=True)]
+        self.time = 0
